b1e312c0862875031efbfd1a59b5f2eb1976cb22
sachinisiriwardene <sachinisiriwardene@gmail.com>
2024-08-14 18:30:38 +0530

    Read the Admin and API Metadata URLs

--- src/prod-server.js
+++ src/prod-server.js
@@ -101,6 +101,14 @@ const ensureAuthenticated = async (req, res, next) => {
 // Middleware to load partials from the database
 app.use(/\/((?!favicon.ico|images).*)/, async (req, res, next) => {
 
+    console.log(req.hostname)
+
+    if(!req.hostname.match("localhost")) {
+        console.log("local")
+
+        config.adminAPI = process.env.AdminURL;
+        config.apiMetaDataAPI = process.env.APIMetaDataURL;
+    }
     const orgName = req.originalUrl.split("/")[1];
     const apiName = req.originalUrl.split("/").pop();
     const url = config.adminAPI + "orgFileType?orgName=" + orgName + "&fileType=partials";
@@ -144,6 +152,13 @@ app.use(/\/((?!favicon.ico|images).*)/, async (req, res, next) => {
 // Route to render Handlebars templates fetched from the database
 router.get('/((?!favicon.ico)):orgName', ensureAuthenticated, async (req, res) => {
 
+    if(!req.hostname.match("localhost")) {
+        config.adminAPI = process.env.AdminURL;
+        config.apiMetaDataAPI = process.env.APIMetaDataURL;
+        console.log("Connection URLs");
+        console.log(config.adminAPI);
+        console.log(config.apiMetaDataAPI);
+    }
     const orgName = req.params.orgName;
     const url = config.adminAPI + "orgFiles?orgName=" + orgName;
     const templateURL = config.adminAPI + "orgFileType?orgName=" + orgName;
@@ -170,6 +185,10 @@ router.get('/((?!favicon.ico)):orgName', ensureAuthenticated, async (req, res) =
 
 router.get('/((?!favicon.ico)):orgName/apis', ensureAuthenticated, async (req, res) => {
 
+    if(!req.hostname.match("localhost")) {
+        config.adminAPI = process.env.AdminURL;
+        config.apiMetaDataAPI = process.env.APIMetaDataURL;
+    }
     const orgName = req.params.orgName;
     const orgFilesUrl = config.adminAPI + "orgFiles?orgName=" + orgName;
     const apiMetaDataUrl = config.apiMetaDataAPI + "apiList?orgName=" + orgName;
@@ -206,6 +225,11 @@ router.get('/((?!favicon.ico)):orgName/apis', ensureAuthenticated, async (req, r
 
 router.get('/((?!favicon.ico)):orgName/api/:apiName', ensureAuthenticated, async (req, res) => {
 
+
+    if(!req.hostname.match("localhost")) {
+        config.adminAPI = process.env.AdminURL;
+        config.apiMetaDataAPI = process.env.APIMetaDataURL;
+    }
     const orgName = req.params.orgName;
     const orgFilesUrl = config.adminAPI + "orgFiles?orgName=" + orgName;
     const apiContetnUrl = config.apiMetaDataAPI + "apiFiles?orgName=" + orgName + "&apiID=" + req.params.apiName;
@@ -245,6 +269,7 @@ router.get('/((?!favicon.ico)):orgName/api/:apiName', ensureAuthenticated, async
 
 router.get('/((?!favicon.ico)):orgName/api/:apiName/tryout', ensureAuthenticated, async (req, res) => {
 
+
     const apiMetaDataUrl = config.apiMetaDataAPI + "apiDefinition?orgName=" + req.params.orgName + "&apiID=" + req.params.apiName;
     const metadataResponse = await fetch(apiMetaDataUrl);
     const metaData = await metadataResponse.text();
@@ -258,6 +283,10 @@ router.get('/((?!favicon.ico)):orgName/api/:apiName/tryout', ensureAuthenticated
 
 router.get('/((?!favicon.ico|images):orgName/*)', ensureAuthenticated, async (req, res) => {
 
+    if(!req.hostname.match("localhost")) {
+        config.adminAPI = process.env.AdminURL;
+        config.apiMetaDataAPI = process.env.APIMetaDataURL;
+    }
     const orgName = req.params.orgName;
     const filePath = req.originalUrl.split(orgName)[1];
     //const markdonwFile = req.params[0].split("/").pop() + ".md";
